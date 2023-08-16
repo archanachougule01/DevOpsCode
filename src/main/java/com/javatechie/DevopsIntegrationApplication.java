9467c050ba174b8cb29b3ec841e1e965c69d4d57
Archana Chougule <archanachougule@GS-5445.local>
2023-08-16 16:26:03 +0530

    Adding project

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "GSLAb Dev Ops Training";
 	}
 
 	public static void main(String[] args) {
